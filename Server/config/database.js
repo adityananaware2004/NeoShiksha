b9146b4fbb42adbdf1ace6818043dfce6f133162
adityananaware2004 <aditya.nanaware.2004@gemail.com>
2025-06-24 23:48:10 +0530

    initial commit

---
+++ Server/config/database.js
@@ -0,0 +1,17 @@
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+exports.connect=() => {
+  mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
+};
