48d97c2b397d0fdbc344cfd0b8f6055f19958c44
adityananaware2004 <aditya.nanaware.2004@gmail.com>
2025-07-03 01:27:53 +0530

    commit

--- Server/controllers/Categories.js
+++ Server/controllers/Categories.js
@@ -1,8 +1,10 @@
 const Category = require("../models/Category")
 const { Mongoose } = require("mongoose");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
 
-
-
+// tag handler
 exports.createCategories = async (req, res) => {
     try{
 
@@ -54,6 +56,90 @@ exports.showAllCategories = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: error.message,
+        })
+    }
+}
+
+// categoryPageDetails handler
+
+exports.categoryPageDetails = async (req, res) => {
+    try{
+        // get courseId
+        const {categoryId} = req.body;
+        //get all courses of this category
+        console.log("categoryId received:", categoryId);
+
+        const selectedCategory = await Category.findById(categoryId)
+                            .populate({
+                                path: "courses",
+                                match: { status: "Published" },
+                                populate: "ratingAndReviews",
+                            })
+                            .exec();
+        
+        // validate the data
+        if(!selectedCategory){
+            return res.status(404).json({
+                success: false,
+                message: "Data not found"
+            })
+        }
+
+        // Handle the case when there are no courses
+        if (selectedCategory.courses.length === 0) {
+            console.log("No courses found for the selected category.")
+            return res.status(404).json({
+            success: false,
+            message: "No courses found for the selected category.",
+            })
+        }
+
+        // Get courses for other categories
+        const categoriesExceptSelected = await Category.find({
+            _id: { $ne: categoryId },
+        })
+
+        let differentCategory = await Category.findOne(
+            categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+              ._id
+        )
+        .populate({
+            path: "courses",
+            match: { status: "Published" },
+        })
+        .exec()
+
+           //console.log("Different COURSE", differentCategory)
+        // Get top-selling courses across all categories
+        const allCategories = await Category.find()
+        .populate({
+            path: "courses",
+            match: { status: "Published" },
+            populate: {
+            path: "instructor",
+        },
+        })
+        .exec()
+
+        const allCourses = allCategories.flatMap((category) => category.courses)
+        const mostSellingCourses = allCourses
+        .sort((a, b) => b.sold - a.sold)
+        .slice(0, 10)
+       // console.log("mostSellingCourses COURSE", mostSellingCourses)
+
+        // return res
+        return res.status(200).json({
+            success: true,
+            message: "Category details fetched successfully",
+            selectedCategory,
+            differentCategory,
+            mostSellingCourses
+        })
+                    
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: error,
         })
     }
 }
