9f90d0497442bba769239260ea648fa2729b2092
adityananaware2004 <aditya.nanaware.2004@gmail.com>
2025-07-03 23:55:12 +0530

    routes done

---
+++ Server/config/razorpay.js
@@ -0,0 +1,10 @@
+
+const Razorpay = require('razorpay');
+require('dotenv').config();
+
+console.log("RAZORPAY_KEY:", process.env.RAZORPAY_KEY);
+
+exports.instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY,
+    key_secret: process.env.RAZORPAY_SECRET
+})
---
+++ Server/routes/Payment.js
@@ -0,0 +1,11 @@
+// Import the required modules
+const express = require("express")
+const router = express.Router()
+
+const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../controllers/Payments")
+const { authenticate, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
+router.post("/capturePayment", authenticate, isStudent, capturePayment)
+router.post("/verifyPayment",authenticate, isStudent, verifyPayment)
+router.post("/sendPaymentSuccessEmail", authenticate, isStudent, sendPaymentSuccessEmail);
+
+module.exports = router
